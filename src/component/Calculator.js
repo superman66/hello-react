c56b6811deb932e1046fde3dc092976d473d418a
superman <supermanchc@gmail.com>
2017-02-28 21:39:14 +0800

    update

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -6,26 +6,57 @@ import BoilingVerdict from './BoilingVerdict'
 import TemperatureInput from './TemperatureInput'
 
 class Calculator extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: ''
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+      scale: 'c'
     }
+  }
 
-    handleChange = (e) => {
-        this.setState({'value': e.target.value})
-    };
+  toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
 
-    render() {
-        const value = this.state.value;
-        return (
-            <div>
-                <TemperatureInput scale="c"/>
-                <TemperatureInput scale="f"/>
-            </div>
-        )
+
+  toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  tryConvert(value, convert) {
+    const input = parseFloat(value);
+    if (Number.isNaN(input)) {
+      return '';
     }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  }
+
+  handleCelsiusChange = (value) => {
+    this.setState({scale: 'c', value})
+  };
+
+  handleFahrenheitChange = (value) => {
+    this.setState({scale: 'f', value})
+  };
+
+
+  render() {
+    const scale = this.state.scale;
+    const value = this.state.value;
+    const celsius = scale === 'f' ? this.tryConvert(value, this.toCelsius) : value;
+    const fahrenheit = scale === 'c' ? this.tryConvert(value, this.toFahrenheit) : value
+
+    return (
+        <div>
+          <TemperatureInput scale="c" value={celsius} onChange={this.handleCelsiusChange}/>
+          <TemperatureInput scale="f" value={fahrenheit} onChange={this.handleFahrenheitChange}/>
+          <BoilingVerdict
+              celsius={parseFloat(celsius)} />
+        </div>
+    )
+  }
 }
 
 export default Calculator;
--- src/component/TemperatureInput.js
+++ src/component/TemperatureInput.js
@@ -1,37 +1,38 @@
 /**
  * Created by superman on 17/2/28.
  */
-import react, {Component} from 'react'
+import React, {Component} from 'react'
 
 const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
+  c: 'Celsius',
+  f: 'Fahrenheit'
 };
 
 class TemperatureInput extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
     }
+  }
 
-    handleChange = (e) => {
-        this.setState({value: e.target.value})
-    }
+  handleChange = (e) => {
+    this.props.onChange(e.target.value);
+  }
 
-    render() {
-        const value = this.props.value;
-        const scale = this.props.scale;
+  render() {
+    const value = this.props.value;
+    const scale = this.props.scale;
 
-        return (
-            <fieldset>
-                <legend>Enter temperature in {scaleNames[scale]}:</legend>
-                <input type="text"
-                       value={valule}
-                       onChange={this.handleChange}
-                />
-            </fieldset>
-        )
-    }
+    return (
+        <fieldset>
+          <legend>Enter temperature in {scaleNames[scale]}:</legend>
+          <input type="text"
+                 value={value}
+                 onChange={this.handleChange}
+          />
+        </fieldset>
+    )
+  }
 }
+export default TemperatureInput
