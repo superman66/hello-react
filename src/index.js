13bb1858d28d27cf31d6ac150f4d02f070286fa2
superman <supermanchc@gmail.com>
2017-03-03 18:17:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory, IndexRoute} from 'react-router'
-import App from './App';
-import Home from './Home'
-import Calculator from './component/Calculator'
-import About from './About'
+import Routes from './routes'
 import './index.css';
 
 ReactDOM.render((
-        <Router history={hashHistory}>
-          <Route path="/" component={App}>
-
-            <IndexRoute component={Home}/>
-
-            <Route path="/about/:username" component={About}></Route>
-            <Route path="/calculator" component={Calculator}></Route>
-          </Route>
-        </Router>
+        Routes
     ),
     document.getElementById('root')
 );
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+/**
+ * Created by superman on 17/3/3.
+ * routes
+ */
+import React from 'react'
+import {Router, Route, hashHistory, IndexRoute} from 'react-router'
+import App from './App'
+import Home from './Home'
+import About from './About'
+import Calculator from './component/Calculator'
+
+export default (
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+
+        <Route path="/about/:username" component={About}/>
+        <Route path='/calculator' component={Calculator}/>
+      </Route>
+    </Router>
+)
